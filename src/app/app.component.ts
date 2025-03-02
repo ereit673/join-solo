a655e24986a1f28877d80f832251eba8c7926d0c
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-03-02 02:04:16 +0100

    added side navigation; created main section component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { SideNavComponent } from "./side-nav/side-nav.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SideNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
