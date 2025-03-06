5985c7bce8bda101ff1f78ae9717e8df22be7bd8
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-03-06 23:16:07 +0100

    styled form

--- src/app/main-section/add-task/add-task.component.ts
+++ src/app/main-section/add-task/add-task.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [FormsModule, DatePipe],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
 export class AddTaskComponent {
+  
 
 }
