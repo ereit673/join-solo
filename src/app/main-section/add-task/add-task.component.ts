63d8b9a697cc2922d4ec56850598040c486707ed
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-03-09 22:53:10 +0100

    updated add task component

--- src/app/main-section/add-task/add-task.component.ts
+++ src/app/main-section/add-task/add-task.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {DatePipe} from '@angular/common';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-add-task',
   imports: [FormsModule, DatePipe],
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.scss'
+  styleUrl: './add-task.component.scss',
 })
 export class AddTaskComponent {
-  
+  dummy_contacts = ['name1', 'name2', 'name3'];
+  selectedPriority = 'medium'; 
 
+  selectPriority(priority: string){
+    this.selectedPriority = priority;
+  }
 }
