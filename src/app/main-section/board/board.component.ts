274bf845d9a9505ca51724f7b12b5e976b7f332f
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-03-22 16:45:38 +0100

    styled card small title

--- src/app/main-section/board/board.component.ts
+++ src/app/main-section/board/board.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NoTaskComponent } from "./no-task/no-task.component";
+import { CardSmallComponent } from "./card-small/card-small.component";
 
 @Component({
   selector: 'app-board',
-  imports: [NoTaskComponent],
+  imports: [NoTaskComponent, CardSmallComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
