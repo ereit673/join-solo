3fde3eb94300a258ec5ce6deb93909b04a3208c5
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-03-25 21:57:37 +0100

    updated big card and made it responsive

--- src/app/main-section/board/board.component.ts
+++ src/app/main-section/board/board.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NoTaskComponent } from "./no-task/no-task.component";
 import { CardSmallComponent } from "./card-small/card-small.component";
+import { CardBigComponent } from "./card-big/card-big.component";
 
 @Component({
   selector: 'app-board',
-  imports: [NoTaskComponent, CardSmallComponent],
+  imports: [NoTaskComponent, CardSmallComponent, CardBigComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
