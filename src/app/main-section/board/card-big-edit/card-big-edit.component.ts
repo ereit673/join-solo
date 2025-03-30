152ee8527505f3de45bee644d7a8ecd57af4d0a7
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-03-30 20:41:55 +0200

    updated edit card big

--- src/app/main-section/board/card-big-edit/card-big-edit.component.ts
+++ src/app/main-section/board/card-big-edit/card-big-edit.component.ts
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './card-big-edit.component.scss'
 })
 export class CardBigEditComponent {
+  selectedPriority = 'medium'; 
+  dummy_contacts = ['name1', 'name2', 'name3'];
+  subtasks = [
+    {description: 'subtask1',
+      id: '1'
+    },
+    {description: 'subtask2',
+      id: '2'
+    },
+    {description: 'subtask3',
+      id: '3'
+    },
+    {description: 'subtask4',
+      id: '4'
+    }
+  ];
+  showContacts = false;
+  showCategories = false;
 
+  selectPriority(priority: string){
+    this.selectedPriority = priority;
+  }
+
+  toggleShowContacts(){
+    this.showContacts = !this.showContacts;
+  }
+
+  toggleShowCategories(){
+    this.showCategories = !this.showCategories;
+  }
 }
--- src/app/main-section/board/card-big/card-big.component.ts
+++ src/app/main-section/board/card-big/card-big.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardBigWrapperComponent } from "../shared/card-big-wrapper/card-big-wrapper.component";
+import { CardBigEditComponent } from "../card-big-edit/card-big-edit.component";
 
 @Component({
   selector: 'app-card-big',
-  imports: [CardBigWrapperComponent],
+  imports: [CardBigWrapperComponent, CardBigEditComponent],
   templateUrl: './card-big.component.html',
   styleUrl: './card-big.component.scss'
 })
